0d7e3ef58c45c082015f83144aec2711c007e34c
Catherine A. Martlin <cmartlin@stsci.edu>
2018-02-19 13:15:56 -0500

    First commit of the logging function project

---
+++ logging/logging_functions.py
@@ -0,0 +1,40 @@
+""" Logging functions for the James Webb Quicklook automation platform
+
+Working notes: 
+1) Will definitely want the same type of decorators that WFC3 QL uses - one
+to track failures and one to track useful system information. Are there
+any other things we will want to create a decorator for? Also which 
+module version information will we want to track with our info decorator?
+
+2) Do we want to follow the same naming convention stuff? Have the make_log_file 
+function preform the same way?
+
+
+
+
+Authors
+-------
+Catherine Martlin 2018
+
+
+Use
+___
+
+
+Dependencies
+____________
+
+
+References
+__________
+
+Code will likely be adopted from python routine written by Alex Viana, 2013 
+for the WFC3 Quicklook automation platform.
+
+Notes
+_____
+
+
+
+"""
+
