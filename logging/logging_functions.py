f4e2afdc9e38615366ad80fa7a65e124b8e9ee2a
Catherine A. Martlin <cmartlin@stsci.edu>
2018-02-21 14:37:18 -0500

    More questions about what we want from this logging script specifically.

--- logging/logging_functions.py
+++ logging/logging_functions.py
@@ -3,11 +3,20 @@
 Working notes: 
 1) Will definitely want the same type of decorators that WFC3 QL uses - one
 to track failures and one to track useful system information. Are there
-any other things we will want to create a decorator for? Also which 
-module version information will we want to track with our info decorator?
+any other things we will want to create a decorator for? 
 
-2) Do we want to follow the same naming convention stuff? Have the make_log_file 
-function preform the same way?
+2) Also which module version information will we want to track with our info 
+decorator?
+
+3) Do we want to follow the same naming convention stuff? Have the make_log_file 
+function perform the same way?
+
+4) Will we follow the same saving convention of the different directories and 
+want to set up a recent directory for the most recent log file to be available 
+all the time? 
+
+
+
 
 
 
