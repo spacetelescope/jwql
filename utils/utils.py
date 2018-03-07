a27c9c7c9470e57f983b566c9e0d5ce5a1549963
Matthew Bourque <bourque@stsci.edu>
2018-03-07 10:19:33 -0500

    Ive decided to just go with JSON format for the config file (for now).

--- utils/utils.py
+++ utils/utils.py
@@ -11,29 +11,23 @@ Use
     This module can be imported as such:
 
     >>> import utils
-    config_yaml, config_json = get_config()
+    settings = get_config()
 """
 
 import json
-import yaml
 
 
 def get_config():
-    """Return dictionaries that hold the contents of the jwql config
-    files.
+    """Return a dictionary that holds the contents of the jwql config
+    file.
 
     Returns
     -------
-    config_yaml : dict
-        A dictionary that holds the contents of the yaml config file.
-    config_json: dict
-        A dictionary that holds the contents of the json config file.
+    settings : dict
+        A dictionary that holds the contents of the config file.
     """
 
-    with open('config.yml', 'r') as f:
-        config_yaml = yaml.load(f)
-
     with open('config.json', 'r') as f:
-        config_json = json.load(f)
+        settings = json.load(f)
 
-    return config_yaml, config_json
+    return settings
