0e120ee6377314005c3dbc6a0239e75ccefba5fa
Matthew Bourque <bourque@stsci.edu>
2018-03-13 14:43:37 -0400

    Added a few more attributes to the setup script.  Initial commit of MANIFEST.in.

--- setup.py
+++ setup.py
@@ -2,19 +2,25 @@ import numpy as np
 from setuptools import setup
 from setuptools import find_packages
 
+VERSION = '0.4.0'
+
 AUTHORS = 'Matthew Bourque, Sara Ogaz, Joe Filippazzo, Bryan Hilbert, Misty Cracraft, Graham Kanarek'
 AUTHORS += 'Johannes Sahlmann, Lauren Chambers'
 
+REQUIRES = ['astropy', 'django', 'matplotlib', 'numpy', 'python-dateutil', 'sphinx', 'sphinx-automodapi', 'sqlalchemy']
+
 setup(
     name = 'jwql',
+    version = VERSION,
     description = 'The JWST Quicklook Project',
     url = 'https://github.com/spacetelescope/jwql.git',
     author = AUTHORS,
-    keywords = ['astronomy'],
+    author_email='jwql@stsci.edu',
+    license='BSD',
+    keywords = ['astronomy', 'python'],
     classifiers = ['Programming Language :: Python'],
     packages = find_packages(),
-    install_requires = ['astropy', 'numpy', 'matplotlib'],
-    version = 0.0,
+    install_requires = REQUIRES,
     include_package_data=True,
     include_dirs = [np.get_include()],
     )
