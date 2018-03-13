c6005c1dd8468f8c79ba25eefaf445a48e428d44
Lauren Chambers <lchambers@stsci.edu>
2018-03-13 19:12:35 -0400

    Integrate preview_image

--- website/apps/plots_example/views.py
+++ website/apps/plots_example/views.py
@@ -1,8 +1,18 @@
+import os
+import sys
+
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
+from django.contrib.staticfiles.templatetags.staticfiles import static
 
 from .models import ImageData
 from .db import DatabaseConnection
+
+# Temporary fix until converted into a package...
+current_dir = os.getcwd()
+parent_dir = os.path.join(os.path.dirname(current_dir), 'preview_image')
+sys.path.insert(0, parent_dir)
+import preview_image
 
 # class IndexView(generic.ListView):
 #     template_name = 'plots_example/index.html'
@@ -15,6 +25,7 @@ from .db import DatabaseConnection
 #                                 order_by('-pub_dat')[:5]
 
 app_name = 'plots_example'
+STATIC_DIR = 'apps/plots_example/static/'
 
 INST_LIST = ['FGS', 'MIRI', 'NIRCam', 'NIRISS', 'NIRSpec']
 TOOLS = {'FGS': ['Bad Pixel Monitor'],
@@ -48,10 +59,33 @@ def instrument(request, inst):
 
 def view_image(request, inst, file):
     template = 'plots_example/view_image.html'
+
+    dirname = file[:7]
+
+    # Check that a jpg does not already exist
+    jpg_filename = os.path.splitext(file)[0] + '_integ0.jpg'
+    # jpg_filepath = static('filesystem_2018_02/' + dirname + jpg_filename)
+    jpg_filepath = os.path.join(STATIC_DIR, 'filesystem_2018_02', dirname, jpg_filename)
+
+    if os.path.exists(jpg_filepath):
+        pass
+
+    # If it doesn't, make it
+    else:
+        fits_filepath = os.path.join(STATIC_DIR, 'filesystem_2018_02', dirname, file)
+
+        # Only process FITS files that are 3D+
+        if fits_filepath[-9:] != 'rate.fits':
+            im = preview_image.Image(fits_filepath, 'SCI')
+            im.make_image()
+        else:
+            jpg_filename = 'Cannot currently create JPEG preview for rate.fits file.'
+
     return render(request, template,
                   {'inst': inst,
                    'file': file,
-                   'tools': TOOLS})
+                   'tools': TOOLS,
+                   'jpg': jpg_filename})
 
 def unlooked_images(request, inst):
     imdat = DatabaseConnection().get_filenames_for_instrument(inst)
