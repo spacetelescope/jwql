f312e0d50131d17fcd893a795ff62d6ec338d8f9
Johannes Sahlmann <jsahlmann@stsci.edu>
2018-06-07 15:00:31 -0400

    Add test script

---
+++ jwql/tests/test_preview_image.py
@@ -0,0 +1,194 @@
+#! /usr/bin/env python
+
+"""Tests for the ``preview_image`` module.
+
+Authors
+-------
+
+    - Johannes Sahlmann
+
+
+Use
+---
+
+    These tests can be run via the command line (omit the ``-s`` to
+    suppress verbose output to ``stdout``):
+
+    ::
+
+        pytest -s test_preview_image.py
+"""
+
+import glob
+import os
+import pytest
+
+from jwql.preview_image.preview_image import PreviewImage
+
+# from jwql.permissions.permissions import set_permissions, has_permissions, \
+#     get_owner_string, get_group_string
+
+# directory to be created and populated during tests running
+TEST_DIRECTORY = os.path.join(os.environ['HOME'], 'preview_image_test')
+
+# directory that contains sample images
+TEST_DATA_DIRECTORY = os.path.join(os.path.abspath(__file__), 'test_data')
+
+@pytest.fixture(scope="module")
+def test_directory(test_dir=TEST_DIRECTORY):
+    """Create a test directory for permission management.
+
+    Parameters
+    ----------
+    test_dir : str
+        Path to directory used for testing
+
+    Yields
+    -------
+    test_dir : str
+        Path to directory used for testing
+    """
+    os.mkdir(test_dir)  # creates directory
+
+    yield test_dir
+    print("teardown test directory")
+    if os.path.isdir(test_dir):
+        os.remove(test_dir)
+
+
+
+
+
+
+
+
+def test_make_image(test_directory):
+    """Create a directory with the standard permissions
+    ``('-rw-r--r--')``.
+
+    Set the default permissions defined in ``permissions.py``. Assert
+    that these were set correctly.
+
+    Parameters
+    ----------
+    test_directory : str
+        Path of directory used for testing
+    """
+    # Get owner and group on the current system.This allows to implement the tests
+    # independently from the user.
+
+
+    filenames = glob.glob(os.path.join(TEST_DATA_DIRECTORY, '*.fits'))
+    print('\nGenerating preview images for {}.'.format(filenames))
+
+    # for filename in filenames:
+    #
+    #     output_directory = os.path.join(preview_image_filesystem, identifier)
+    #     thumbnail_output_directory = os.path.join(thumbnail_filesystem, identifier)
+    #
+    #     # Create the output directories if necessary
+    #     if not os.path.exists(output_directory):
+    #         os.makedirs(output_directory)
+    #         permissions.set_permissions(output_directory, verbose=False)
+    #     if not os.path.exists(thumbnail_output_directory):
+    #         os.makedirs(thumbnail_output_directory)
+    #         permissions.set_permissions(thumbnail_output_directory, verbose=False)
+    #
+    #     # Create and save the preview image and thumbnail
+    #     args = zip((False, True), (output_directory, thumbnail_output_directory))
+    #     for thumbnail_bool, directory in args:
+    #         try:
+    #             im = PreviewImage(filename, "SCI")
+    #             im.clip_percent = 0.01
+    #             im.scaling = 'log'
+    #             im.cmap = 'viridis'
+    #             im.output_format = 'jpg'
+    #             im.thumbnail = thumbnail_bool
+    #             im.output_directory = directory
+    #             im.make_image()
+    #         except ValueError as error:
+    #             print(error)
+
+    # owner = get_owner_string(test_directory)
+    # group = get_group_string(test_directory)
+    # print('\nCurrent owner={} group={}'.format(owner, group))
+    #
+    # set_permissions(test_directory, owner=owner, group=group)
+    # assert has_permissions(test_directory, owner=owner, group=group)
+
+
+# @pytest.fixture()
+# def test_file(test_dir=TEST_DIRECTORY):
+#     """Create a test file for permission management.
+#
+#     Parameters
+#     ----------
+#     test_dir : str
+#         Path to directory used for testing
+#
+#     Yields
+#     -------
+#     filename : str
+#         Path of file used for testing
+#     """
+#     if not os.path.isdir(test_dir):
+#         os.mkdir(test_dir)
+#
+#     filename = os.path.join(test_dir, 'permission_test.txt')
+#     with open(filename, 'w') as filestream:
+#         filestream.write('jwql permission test')
+#     yield filename
+#     print("teardown test file and directory ")
+#     if os.path.isfile(filename):
+#         os.remove(filename)
+#     if os.path.isdir(test_dir):
+#         os.rmdir(test_dir)
+#
+#
+# # @pytest.mark.xfail
+# def test_file_group(test_file):
+#     """Create a file with the standard permissions ``('-rw-r--r--')``
+#     and default group.
+#
+#     Modify the group and set the default permissions defined in
+#     ``permissions.py``.  Assert that both group and permissions were
+#     set correctly.
+#
+#     Parameters
+#     ----------
+#     test_file : str
+#         Path of file used for testing
+#     """
+#     # Get owner and group on the current system.
+#     owner = get_owner_string(test_file)
+#     group = get_group_string(test_file)
+#
+#     # attempt to retrieve a group name different from default
+#     group_index = 0
+#     test_group = grp.getgrgid(os.getgroups()[group_index]).gr_name
+#
+#     set_permissions(test_file, group=test_group, owner=owner)
+#     assert has_permissions(test_file, group=test_group, owner=owner)
+#
+#     # return to default group
+#     set_permissions(test_file, owner=owner, group=group)
+#     assert has_permissions(test_file, owner=owner, group=group)
+#
+#
+# def test_file_permissions(test_file):
+#     """Create a file with the standard permissions ``('-rw-r--r--')``.
+#
+#     Set the default permissions defined in ``permissions.py``. Assert
+#     that these were set correctly.
+#
+#     Parameters
+#     ----------
+#     test_file : str
+#         Path of file used for testing
+#     """
+#     # Get owner and group on the current system.
+#     owner = get_owner_string(test_file)
+#     group = get_group_string(test_file)
+#
+#     set_permissions(test_file, owner=owner, group=group)
+#     assert has_permissions(test_file, owner=owner, group=group)
