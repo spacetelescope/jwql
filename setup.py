b7e4aeb7c6892b90200a4a54321ad0a1f2c550fe
Matthew Bourque <bourque@stsci.edu>
2018-06-20 12:04:56 -0400

    Added function to get install_requires directly from the environment.yml file.

--- setup.py
+++ setup.py
@@ -2,12 +2,45 @@ import numpy as np
 from setuptools import setup
 from setuptools import find_packages
 
+
 VERSION = '0.4.0'
 
 AUTHORS = 'Matthew Bourque, Sara Ogaz, Joe Filippazzo, Bryan Hilbert, Misty Cracraft, Graham Kanarek'
 AUTHORS += 'Johannes Sahlmann, Lauren Chambers, Catherine Martlin'
 
-REQUIRES = ['astropy', 'astroquery', 'bokeh==0.12.5', 'django', 'matplotlib', 'numpy', 'python-dateutil', 'sphinx', 'sphinx-automodapi', 'sqlalchemy']
+
+def get_requires():
+    """Reads in the ``environment.yml`` file to determine the value of
+    the ``install_requires`` parameter.
+
+    Returns
+    -------
+    requires : list
+        The list of requirements.
+    """
+
+    # Read in the environment file
+    with open('environment.yml') as f:
+        environment_file = f.readlines()
+
+    # Determine where list of dependencies begin
+    index = 0
+    for line in environment_file:
+        index += 1
+        if 'dependencies:' in line:
+            break
+    dependencies = environment_file[index:]
+
+    # Remove excluded libraries
+    exclude_list = ['pip:', 'python', 'postgresql']
+    for library in exclude_list:
+        dependencies = [item for item in dependencies if library not in item]
+
+    # Parse out the remaining library names
+    requires = [item.split('=')[0].split()[-1] for item in dependencies]
+
+    return requires
+
 
 setup(
     name='jwql',
@@ -20,7 +53,7 @@ setup(
     keywords=['astronomy', 'python'],
     classifiers=['Programming Language :: Python'],
     packages=find_packages(),
-    install_requires=REQUIRES,
+    install_requires=get_requires(),
     include_package_data=True,
     include_dirs=[np.get_include()],
     )
