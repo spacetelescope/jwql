abe3ecd4d301efc8e1308b1f97d1f6cddbe99cc6
Matthew Bourque <bourque@stsci.edu>
2018-06-20 11:30:17 -0400

    Forgot that two equal signs are required :(

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ VERSION = '0.4.0'
 AUTHORS = 'Matthew Bourque, Sara Ogaz, Joe Filippazzo, Bryan Hilbert, Misty Cracraft, Graham Kanarek'
 AUTHORS += 'Johannes Sahlmann, Lauren Chambers, Catherine Martlin'
 
-REQUIRES = ['astropy', 'astroquery', 'bokeh==0.12.5', 'django=2.0.5', 'matplotlib', 'numpy', 'python-dateutil', 'sphinx', 'sphinx-automodapi', 'sqlalchemy']
+REQUIRES = ['astropy', 'astroquery', 'bokeh==0.12.5', 'django==2.0.5', 'matplotlib', 'numpy', 'python-dateutil', 'sphinx', 'sphinx-automodapi', 'sqlalchemy']
 
 setup(
     name='jwql',
