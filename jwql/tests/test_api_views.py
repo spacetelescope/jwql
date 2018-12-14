479cfe28f88a56d6f25f59095837bb1d365cfe9d
Matthew Bourque <bourque@stsci.edu>
2018-12-14 13:55:33 -0500

    Initial commit of test_api_views, which provides tests for the api_views module in the jwql web app.

---
+++ jwql/tests/test_api_views.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+
+"""Tests for the ``api_views`` module in the ``jwql`` web application.
+
+Authors
+-------
+
+    - Matthew Bourque
+
+Use
+---
+
+    These tests can be run via the command line (omit the -s to
+    suppress verbose output to stdout):
+
+    ::
+
+        pytest -s test_api_views.py
+"""
+
+import getpass
+import json
+import pytest
+import urllib.request
+
+from jwql.utils.utils import get_config
+
+
+# Determine if this module is being run in production or locally
+username = getpass.getuser()
+if username == get_config()['admin_account']:
+    base_url = 'https://dljwql.stsci.edu'
+else:
+    base_url = 'http://127.0.0.1:8000'
+
+urls = [
+    'api/proposals/',  # all_proposals
+    'api/86700/filenames/',  # filenames_by_proposal
+    'api/jw86700005001_02101_00001_guider1/filenames/',  # filenames_by_rootname
+    'api/fgs/proposals/',  # instrument_proposals
+    'api/fgs/preview_images/',  # preview_images_by_instrument
+    'api/86700/preview_images/',  # preview_images_by_proposal
+    'api/jw86700005001_02101_00001_guider1/preview_images/',  # preview_images_by_rootname
+    'api/fgs/thumbnails/',  # thumbnails_by_instrument
+    'api/86700/thumbnails/',  # thumbnails_by_proposal
+    'api/jw86700005001_02101_00001_guider1/thumbnails/'  # thumbnails_by_rootname
+    ]
+urls = ['{}/{}'.format(base_url, url) for url in urls]
+
+
+@pytest.mark.parametrize('url', urls)
+def test_api_view(url):
+    """Test to see if the given ``url`` returns a populated JSON object
+
+    Parameters
+    ----------
+    url : str
+        The url to the api view of interest (e.g.
+        ``http://127.0.0.1:8000/api/86700/filenames/'``).
+    """
+
+    # Determine the type of data to check for based on the url
+    data_type = url.split('/')[-2]
+
+    url = urllib.request.urlopen(url)
+    data = json.loads(url.read().decode())
+
+    assert len(data[data_type]) > 0
