65a23ac28c90b761279448877034cd159697e2fa
Bryan Hilbert <hilbert@stsci.edu>
2019-02-13 10:38:56 -0500

    Add __version__ and version badge to README

--- jwql/__init__.py
+++ jwql/__init__.py
@@ -1,20 +1,9 @@
-import os.path
-from pkg_resources import get_distribution, DistributionNotFound
-
-try:
-    _dist = get_distribution('jwql')
-    # Normalize case for Windows systems
-    dist_loc = os.path.normcase(_dist.location)
-    here = os.path.normcase(__file__)
-
-    print(type(_dist))
-    print(_dist.__dict__)
-
-
-    if not here.startswith(os.path.join(dist_loc, 'jwql')):
-        # not installed, but there is another version that *is*
-        raise DistributionNotFound
-except DistributionNotFound:
-    __version__ = 'Please install this project with setup.py'
-else:
-    __version__ = _dist.version
+import os
+import pkg_resources
+module_path = pkg_resources.resource_filename('jwql', '')
+setup_path = os.path.normpath(os.path.join(module_path, '../setup.py'))
+with open(setup_path) as f:
+    data = f.readlines()
+for line in data:
+    if 'VERSION =' in line:
+        __version__ = line.split(' ')[-1].replace("'", "").strip()
