aebcb586bbb6dade5e6636b74f90e42f6c6e9464
Matthew Bourque <bourque@stsci.edu>
2019-02-14 17:23:30 -0500

    Minor code formatting changes

--- jwql/__init__.py
+++ jwql/__init__.py
@@ -1,9 +1,12 @@
 import os
 import pkg_resources
+
 module_path = pkg_resources.resource_filename('jwql', '')
 setup_path = os.path.normpath(os.path.join(module_path, '../setup.py'))
+
 with open(setup_path) as f:
     data = f.readlines()
+
 for line in data:
     if 'VERSION =' in line:
         __version__ = line.split(' ')[-1].replace("'", "").strip()
