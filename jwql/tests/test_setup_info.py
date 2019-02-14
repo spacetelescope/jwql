52071842e121a3568d9a10b83b14a86399d25448
Bryan Hilbert <hilbert@stsci.edu>
2019-02-14 12:43:37 -0500

    Add simple test. Remove development lines.

---
+++ jwql/tests/test_setup_info.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python
+"""Tests for the ``setup.py`` module.
+
+Authors
+-------
+
+    - Bryan Hilbert
+
+
+Use
+---
+
+    These tests can be run via the command line (omit the ``-s`` to
+    suppress verbose output to ``stdout``):
+
+    ::
+
+        pytest -s test_setup_info.py
+"""
+import pytest
+
+
+def test_version_number():
+    """Test that the JWQL version number is retrieved from setup.py"""
+    import jwql
+    assert isinstance(jwql.__version__, str)
+    version_parts = jwql.__version__.split('.')
+    assert len(version_parts) == 3
