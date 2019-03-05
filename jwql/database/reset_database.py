c85ce666bdfd550a068937816103bbe881d182ec
Matthew Bourque <bourque@stsci.edu>
2019-03-05 12:12:46 -0500

    Added check to avoid resetting the production database.

--- jwql/database/reset_database.py
+++ jwql/database/reset_database.py
@@ -19,7 +19,9 @@ Dependencies
 ------------
 
     Users must have a ``config.json`` configuration file with a proper
-    ``connection_string`` key that points to the ``jwqldb`` database
+    ``connection_string`` key that points to the ``jwqldb`` database.
+    The ``connection_string`` format is
+    ``postgresql+psycopg2://user:password@host:port/database``.
 """
 
 from jwql.database.database_interface import base
@@ -29,6 +31,9 @@ from jwql.utils.utils import get_config
 if __name__ == '__main__':
 
     connection_string = get_config()['connection_string']
+    server_type = connection_string.split('@')[-1][0]
+
+    assert server_type != 'p', 'Cannot reset production database!'
 
     prompt = ('About to reset all tables for database instance {}. Do you '
               'wish to proceed? (y/n)\n'.format(connection_string))
