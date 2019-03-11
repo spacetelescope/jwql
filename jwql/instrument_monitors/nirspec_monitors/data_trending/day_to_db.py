b9c5a70ef64ef7385040512d9e9a5b213d78af0c
DanielKuebi <daniflug95@live.at>
2019-03-11 09:53:57 -0400

    checkout for presentation

--- jwql/instrument_monitors/nirspec_monitors/data_trending/day_to_db.py
+++ jwql/instrument_monitors/nirspec_monitors/data_trending/day_to_db.py
@@ -1,41 +1,30 @@
-import os
 import statistics
-import sqlite3
-
+import os
+import jwql.instrument_monitors.nirspec_monitors.data_trending.utils.mnemonics as mn
+import jwql.instrument_monitors.nirspec_monitors.data_trending.utils.sql_interface as sql
+import jwql.instrument_monitors.nirspec_monitors.data_trending.utils.csv_to_AstropyTable as apt
 from jwql.utils.utils import get_config, filename_parser
 
-import jwql.instrument_monitors.miri_monitors.data_trending.utils.mnemonics as mn
-import jwql.instrument_monitors.miri_monitors.data_trending.utils.sql_interface as sql
-import jwql.instrument_monitors.miri_monitors.data_trending.utils.csv_to_AstropyTable as apt
-from jwql.instrument_monitors.miri_monitors.data_trending.utils.process_data import whole_day_routine, wheelpos_routine
+from astropy.table import Table, Column
 
+from jwql.instrument_monitors.nirspec_monitors.data_trending.utils.process_data import once_a_day_routine
 
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
-#files with data to initially fill the database
-filenames = [
-'imir_190204_DoY2292017FOFTLM2019035182609402.CSV',
-'imir_190204_DoY2402017FOFTLM2019035180907145.CSV',
-'imir_190204_DoY2412017FOFTLM2019035181004311.CSV',
-'imir_190204_DoY2312017FOFTLM2019035184159965.CSV',
-'imir_190204_DoY2422017FOFTLM2019035181027504.CSV',
-'imir_190204_DoY2322017FOFTLM2019035184236985.CSV',
-'imir_190204_DoY2432017FOFTLM2019035181100606.CSV',
-'imir_190204_DoY2332017FOFTLM2019035184306076.CSV',
-'imir_190204_DoY2442017FOFTLM2019035181126853.CSV',
-'imir_190204_DoY2342017FOFTLM2019035184347174.CSV',
-'imir_190204_DoY2452017FOFTLM2019035181155234.CSV',
-'imir_190204_DoY2352017FOFTLM2019035184708935.CSV',
-'imir_190204_DoY2462017FOFTLM2019035181230871.CSV',
-'imir_190204_DoY2362017FOFTLM2019035184737246.CSV',
-'imir_190204_DoY2472017FOFTLM2019035181252890.CSV',
-'imir_190204_DoY2372017FOFTLM2019035184806338.CSV',
-'imir_190204_DoY2482017FOFTLM2019035181322838.CSV',
-'imir_190204_DoY2382017FOFTLM2019035184832737.CSV',
-'imir_190204_DoY2492017FOFTLM2019035181406861.CSV',
-'imir_190204_DoY2392017FOFTLM2019035180833486.CSV',
-'imir_190204_DoY2502017FOFTLM2019035181519288.CSV']
+#point to the directory where your files are located!
+directory = '/home/daniel/STScI/trainigData/nirspec_day/'
 
+#here some some files contain the same data but they are all incomplete
+#in order to generate a full database we have to import all of them
+filenames = [
+"DOY_234_15m_chopped.CSV",
+"DOY_236_15m_chopped.CSV",
+"DOY_238_15m_chopped.CSV",
+"DOY_240_15m_chopped.CSV",
+"DOY_235_15m_chopped.CSV",
+"DOY_237_15m_chopped.CSV",
+"DOY_239_15m_chopped.CSV",
+"DOY_241_15m_chopped.CSV"]
 
 def process_file(conn, path):
     '''Parse CSV file, process data within and put to DB
@@ -47,119 +36,101 @@ def process_file(conn, path):
         defines path to the files
     '''
 
+    #import mnemonic data and append dict to variable below
     m_raw_data = apt.mnemonics(path)
 
-    cond3, FW_volt, GW14_volt, GW23_volt, CCC_volt = whole_day_routine(m_raw_data)
-    FW, GW14, GW23, CCC= wheelpos_routine(m_raw_data)
+    #process raw data with once a day routine
+    cond1, cond2, cond3, cond4 = whole_day_routine(m_raw_data)
 
-    #put data from con3 to database
+    #put all data in a database that uses a condition
+    for key, value in cond1.items():
+
+        m = m_raw_data.mnemonic(key)
+
+        length = len(value)
+        mean = statistics.mean(value)
+        deviation = statistics.stdev(value)
+
+        dataset = (float(m.meta['start']), float(m.meta['end']), length, mean, deviation)
+        sql.add_data(conn, key, dataset)
+
+
+    for key, value in cond2.items():
+
+        m = m_raw_data.mnemonic(key)
+
+        length = len(value)
+        mean = statistics.mean(value)
+        deviation = statistics.stdev(value)
+
+        dataset = (float(m.meta['start']), float(m.meta['end']), length, mean, deviation)
+        sql.add_data(conn, key, dataset)
+
+
     for key, value in cond3.items():
 
         m = m_raw_data.mnemonic(key)
 
-        if value != None:
-            if len(value) > 2:
-                if key == "SE_ZIMIRICEA":
-                    length = len(value)
-                    mean = statistics.mean(value)
-                    deviation = statistics.stdev(value)
-                    dataset = (float(m.meta['start']), float(m.meta['end']), length, mean, deviation)
-                    sql.add_data(conn, "SE_ZIMIRICEA_HV_ON", dataset)
+        length = len(value)
+        mean = statistics.mean(value)
+        deviation = statistics.stdev(value)
 
-                elif key == "IMIR_HK_ICE_SEC_VOLT4":
-                    length = len(value)
-                    mean = statistics.mean(value)
-                    deviation = statistics.stdev(value)
-                    dataset = (float(m.meta['start']), float(m.meta['end']), length, mean, deviation)
-                    sql.add_data(conn, "IMIR_HK_ICE_SEC_VOLT4_HV_ON", dataset)
+        dataset = (float(m.meta['start']), float(m.meta['end']), length, mean, deviation)
+        sql.add_data(conn, key, dataset)
 
-                else:
-                    length = len(value)
-                    mean = statistics.mean(value)
-                    deviation = statistics.stdev(value)
-                    dataset = (float(m.meta['start']), float(m.meta['end']), length, mean, deviation)
-                    sql.add_data(conn, key, dataset)
-###########################################################################
 
-    m = m_raw_data.mnemonic('IMIR_HK_FW_POS_VOLT')
-    if FW_volt != None:
-        if len(FW_volt) > 2:
-            length = len(FW_volt)
-            mean = statistics.mean(FW_volt)
-            deviation = statistics.stdev(FW_volt)
+    for key, value in cond4.items():
+
+        m = m_raw_data.mnemonic(key)
+
+        length = len(value)
+        mean = statistics.mean(value)
+        deviation = statistics.stdev(value)
+
+        dataset = (float(m.meta['start']), float(m.meta['end']), length, mean, deviation)
+        sql.add_data(conn, key, dataset)
+
+
+
+    #add rest of the data to database
+    for identifier in mn.mnemSet_day:
+
+        m = m_raw_data.mnemonic(identifier)
+
+        temp = []
+
+        #look for all values that fit to the given conditions
+        for element in m:
+            temp.append(float(element['value']))
+
+        #return None if no applicable data was found
+        if len(temp) > 2:
+            length = len(temp)
+            mean = statistics.mean(temp)
+            deviation = statistics.stdev(temp)
+
             dataset = (float(m.meta['start']), float(m.meta['end']), length, mean, deviation)
-            sql.add_data(conn, "IMIR_HK_FW_POS_VOLT", dataset)
+            sql.add_data(conn, identifier, dataset)
+        else:
+            print('No data for {}'.format(identifier))
 
-    m = m_raw_data.mnemonic('IMIR_HK_GW14_POS_VOLT')
-    if GW14_volt != None:
-        if len(GW14_volt) > 2:
-            length = len(GW14_volt)
-            mean = statistics.mean(GW14_volt)
-            deviation = statistics.stdev(GW14_volt)
-            dataset = (float(m.meta['start']), float(m.meta['end']), length, mean, deviation)
-            sql.add_data(conn, "IMIR_HK_GW14_POS_VOLT", dataset)
-
-    m = m_raw_data.mnemonic('IMIR_HK_GW23_POS_VOLT')
-    if GW23_volt != None:
-        if len(GW23_volt) > 2:
-            length = len(GW23_volt)
-            mean = statistics.mean(GW23_volt)
-            deviation = statistics.stdev(GW23_volt)
-            dataset = (float(m.meta['start']), float(m.meta['end']), length, mean, deviation)
-            sql.add_data(conn, "IMIR_HK_GW23_POS_VOLT", dataset)
-
-    m = m_raw_data.mnemonic('IMIR_HK_CCC_POS_VOLT')
-    if CCC_volt != None:
-        if len(CCC_volt) > 2:
-            length = len(CCC_volt)
-            mean = statistics.mean(CCC_volt)
-            deviation = statistics.stdev(CCC_volt)
-            dataset = (float(m.meta['start']), float(m.meta['end']), length, mean, deviation)
-            sql.add_data(conn, "IMIR_HK_CCC_POS_VOLT", dataset)
-
-    #########################################################################################
-    for pos in mn.fw_positions:
-        try:
-            data = FW[pos]
-            for element in data:
-                sql.add_wheel_data(conn, 'IMIR_HK_FW_POS_RATIO_{}'.format(pos), element)
-        except KeyError:
-            pass
-
-    for pos in mn.gw_positions:
-        try:
-            data_GW14 = GW14[pos]
-            data_GW23 = GW23[pos]
-
-            for element in data_GW14:
-                sql.add_wheel_data(conn, 'IMIR_HK_GW14_POS_RATIO_{}'.format(pos), element)
-            for element in data_GW23:
-                sql.add_wheel_data(conn, 'IMIR_HK_GW23_POS_RATIO_{}'.format(pos), element)
-        except KeyError:
-            pass
-
-    for pos in mn.ccc_positions:
-        try:
-            data = CCC[pos]
-            for element in data:
-                sql.add_wheel_data(conn, 'IMIR_HK_CCC_POS_RATIO_{}'.format(pos), element)
-        except KeyError:
-            pass
+        del temp
 
 
 def main():
-    #point to database
+    #generate paths
     DATABASE_LOCATION = os.path.join(get_config()['jwql_dir'], 'database')
-    DATABASE_FILE = os.path.join(DATABASE_LOCATION, 'miri_database.db')
+    DATABASE_FILE = os.path.join(DATABASE_LOCATION, 'nirspec_database.db')
 
     #connect to temporary database
     conn = sql.create_connection(DATABASE_FILE)
 
-    #process all files in filenames
+    #do for every file in list above
     for name in filenames:
         path = directory + name
         process_file(conn, path)
 
+    #close connection
     sql.close_connection(conn)
     print("done")
 
