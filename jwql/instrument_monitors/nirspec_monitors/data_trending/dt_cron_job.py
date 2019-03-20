33458202f5d2c77fa69b25e1333af5262807c907
DanielKuebi <daniflug95@live.at>
2019-03-20 14:41:25 -0400

    actualized cron job

--- jwql/instrument_monitors/nirspec_monitors/data_trending/dt_cron_job.py
+++ jwql/instrument_monitors/nirspec_monitors/data_trending/dt_cron_job.py
@@ -36,174 +36,123 @@ import sqlite3
 
 from astropy.time import Time
 
-def add_day_to_db(conn, m_raw_data):
-    '''Process 'one_day' data and add to miri_database
+
+def process_daysample(conn, m_raw_data):
+    '''Parse CSV file, process data within and put to DB
     Parameters
     ----------
     conn : DBobject
-        connection object to specific database
-    m_raw_data : dict
-        holds astropy table of data with corresponding mnemonic as key
+        Connection object to temporary database
+    path : str
+        defines path to the files
     '''
 
-    #process data with predefined routines
-    cond3, FW_volt, GW14_volt, GW23_volt, CCC_volt = whole_day_routine(m_raw_data)
-    FW, GW14, GW23, CCC= wheelpos_routine(m_raw_data)
+    #process raw data with once a day routine
+    return_data, lamp_data = whole_day_routine(m_raw_data)
+    FW, GWX, GWY = wheelpos_routine(m_raw_data)
 
-    #put data from con3 to database
-    for key, value in cond3.items():
-
-        #abbreviate astropy table of dict(key)
+    #put all data to a database that uses a condition
+    for key, value in return_data.items():
         m = m_raw_data.mnemonic(key)
-
-        #Add data to database
-        #######################################################################
-        if value != None:
-            if len(value) > 2:
-                if key == "SE_ZIMIRICEA":
-                    length = len(value)
-                    mean = statistics.mean(value)
-                    deviation = statistics.stdev(value)
-                    dataset = (float(m.meta['start']), float(m.meta['end']), length, mean, deviation)
-                    sql.add_data(conn, "SE_ZIMIRICEA_HV_ON", dataset)
-
-                elif key == "IMIR_HK_ICE_SEC_VOLT4":
-                    length = len(value)
-                    mean = statistics.mean(value)
-                    deviation = statistics.stdev(value)
-                    dataset = (float(m.meta['start']), float(m.meta['end']), length, mean, deviation)
-                    sql.add_data(conn, "IMIR_HK_ICE_SEC_VOLT4_HV_ON", dataset)
-
-                else:
-                    length = len(value)
-                    mean = statistics.mean(value)
-                    deviation = statistics.stdev(value)
-                    dataset = (float(m.meta['start']), float(m.meta['end']), length, mean, deviation)
-                    sql.add_data(conn, key, dataset)
-
-
-    m = m_raw_data.mnemonic('IMIR_HK_FW_POS_VOLT')
-    if FW_volt != None:
-        if len(FW_volt) > 2:
-            length = len(FW_volt)
-            mean = statistics.mean(FW_volt)
-            deviation = statistics.stdev(FW_volt)
-            dataset = (float(m.meta['start']), float(m.meta['end']), length, mean, deviation)
-            sql.add_data(conn, "IMIR_HK_FW_POS_VOLT", dataset)
-
-    m = m_raw_data.mnemonic('IMIR_HK_GW14_POS_VOLT')
-    if GW14_volt != None:
-        if len(GW14_volt) > 2:
-            length = len(GW14_volt)
-            mean = statistics.mean(GW14_volt)
-            deviation = statistics.stdev(GW14_volt)
-            dataset = (float(m.meta['start']), float(m.meta['end']), length, mean, deviation)
-            sql.add_data(conn, "IMIR_HK_GW14_POS_VOLT", dataset)
-
-    m = m_raw_data.mnemonic('IMIR_HK_GW23_POS_VOLT')
-    if GW23_volt != None:
-        if len(GW23_volt) > 2:
-            length = len(GW23_volt)
-            mean = statistics.mean(GW23_volt)
-            deviation = statistics.stdev(GW23_volt)
-            dataset = (float(m.meta['start']), float(m.meta['end']), length, mean, deviation)
-            sql.add_data(conn, "IMIR_HK_GW23_POS_VOLT", dataset)
-
-    m = m_raw_data.mnemonic('IMIR_HK_CCC_POS_VOLT')
-    if CCC_volt != None:
-        if len(CCC_volt) > 2:
-            length = len(CCC_volt)
-            mean = statistics.mean(CCC_volt)
-            deviation = statistics.stdev(CCC_volt)
-            dataset = (float(m.meta['start']), float(m.meta['end']), length, mean, deviation)
-            sql.add_data(conn, "IMIR_HK_CCC_POS_VOLT", dataset)
-
-    #Add wheelposition data to database
-    ###########################################################################
-    for pos in mn.fw_positions:
-        try:
-            data = FW[pos]
-            for element in data:
-                sql.add_wheel_data(conn, 'IMIR_HK_FW_POS_RATIO_{}'.format(pos), element)
-        except KeyError:
-            pass
-
-    for pos in mn.gw_positions:
-        try:
-            data_GW14 = GW14[pos]
-            data_GW23 = GW14[pos]
-
-            for element in data_GW14:
-                sql.add_wheel_data(conn, 'IMIR_HK_GW14_POS_RATIO_{}'.format(pos), element)
-            for element in data_GW23:
-                sql.add_wheel_data(conn, 'IMIR_HK_GW23_POS_RATIO_{}'.format(pos), element)
-        except KeyError:
-            pass
-
-    for pos in mn.ccc_positions:
-        try:
-            data = CCC[pos]
-            for element in data:
-                sql.add_wheel_data(conn, 'IMIR_HK_CCC_POS_RATIO_{}'.format(pos), element)
-        except KeyError:
-            pass
-
-
-def add_15min_to_db(conn, m_raw_data):
-    '''Process '15min' data and add to miri_database
-    Parameters
-    ----------
-    conn : DBobject
-        connection object to specific database
-    m_raw_data : dict
-        holds astropy table of data with corresponding mnemonic as key
-    '''
-
-    #process data
-    cond1, cond2 = once_a_day_routine(m_raw_data)
-
-    #Add data to database
-    ############################################################################
-    for key, value in cond1.items():
-
-        #abbreviate astropy table of dict(key)
-        m = m_raw_data.mnemonic(key)
-
-        if key == "SE_ZIMIRICEA":
-            length = len(value)
-            mean = statistics.mean(value)
-            deviation = statistics.stdev(value)
-
-            dataset = (float(m.meta['start']), float(m.meta['end']), length, mean, deviation)
-            sql.add_data(conn, "SE_ZIMIRICEA_IDLE", dataset)
-
-        elif key == "IMIR_HK_ICE_SEC_VOLT4":
-            length = len(value)
-            mean = statistics.mean(value)
-            deviation = statistics.stdev(value)
-
-            dataset = (float(m.meta['start']), float(m.meta['end']), length, mean, deviation)
-            sql.add_data(conn, "IMIR_HK_ICE_SEC_VOLT4_IDLE", dataset)
-
-        else:
-            length = len(value)
-            mean = statistics.mean(value)
-            deviation = statistics.stdev(value)
-
-            dataset = (float(m.meta['start']), float(m.meta['end']), length, mean, deviation)
-            sql.add_data(conn, key, dataset)
-
-    for key, value in cond2.items():
         length = len(value)
         mean = statistics.mean(value)
         deviation = statistics.stdev(value)
-
         dataset = (float(m.meta['start']), float(m.meta['end']), length, mean, deviation)
         sql.add_data(conn, key, dataset)
 
 
+    #add rest of the data to database -> no conditions applied
+    for identifier in mn.mnemSet_day:
+        m = m_raw_data.mnemonic(identifier)
+        temp = []
+        #look for all values that fit to the given conditions
+        for element in m:
+            temp.append(float(element['value']))
+        #return None if no applicable data was found
+        if len(temp) > 2:
+            length = len(temp)
+            mean = statistics.mean(temp)
+            deviation = statistics.stdev(temp)
+        else:
+            print('No data for {}'.format(identifier))
+        del temp
+
+    #add lamp data to database -> distiction over lamps
+    for key, values in lamp_data.items():
+        for data in values:
+            dataset_volt = (data[0], data[1], data[5], data[6], data[7])
+            dataset_curr = (data[0], data[1], data[2], data[3], data[4])
+            sql.add_data(conn, 'LAMP_{}_VOLT'.format(key), dataset_volt)
+            sql.add_data(conn, 'LAMP_{}_CURR'.format(key), dataset_curr)
+
+
+    #add wheeldata to database
+    for key, values in FW.items():
+        for data in values:
+            sql.add_wheel_data(conn, 'INRSI_C_FWA_POSITION_{}'.format(key), data)
+
+    for key, values in GWX.items():
+        for data in values:
+            sql.add_wheel_data(conn, 'INRSI_C_GWA_X_POSITION_{}'.format(key), data)
+
+    for key, values in GWY.items():
+        for data in values:
+            sql.add_wheel_data(conn, 'INRSI_C_GWA_Y_POSITION_{}'.format(key), data)
+
+
+def process_15minsample(conn, m_raw_data):
+    '''Parse CSV file, process data within and put to DB
+    Parameters
+    ----------
+    conn : DBobject
+        Connection object to temporary database
+    path : str
+        defines path to the files
+    '''
+
+    #process raw data with once a day routine
+    returndata = once_a_day_routine(m_raw_data)
+
+    #put all data in a database that uses a condition
+    for key, value in returndata.items():
+        m = m_raw_data.mnemonic(key)
+        length = len(value)
+        mean = statistics.mean(value)
+        deviation = statistics.stdev(value)
+        dataset = (float(m.meta['start']), float(m.meta['end']), length, mean, deviation)
+        sql.add_data(conn, key, dataset)
+
+    #add rest of the data to database
+    for identifier in mn.mnemSet_15min:
+
+        m = m_raw_data.mnemonic(identifier)
+
+        temp = []
+
+        #look for all values that fit to the given conditions
+        for element in m:
+            temp.append(float(element['value']))
+
+        #return None if no applicable data was found
+        if len(temp) > 2:
+            length = len(temp)
+            mean = statistics.mean(temp)
+            deviation = statistics.stdev(temp)
+
+            dataset = (float(m.meta['start']), float(m.meta['end']), length, mean, deviation)
+            sql.add_data(conn, identifier, dataset)
+        elif len(temp) == 2:
+            dataset = (float(element['time']), float(element['time']), 1, temp[0], 0)
+            sql.add_data(conn, identifier, dataset)
+        else:
+            print('No data for {}'.format(identifier))
+            print(temp)
+
+        del temp
+
 def main():
 
+    '''
     from ..utils.engineering_database import query_single_mnemonic
 
     mnemonic_identifier = 'SA_ZFGOUTFOV'
@@ -213,7 +162,7 @@ def main():
 
     mnemonic = query_single_mnemonic(mnemonic_identifier, start_time, end_time)
     assert len(mnemonic.data) == mnemonic.meta['paging']['rows']
-
+    '''
 
 
     for mnemonic in mn.mnemonic_set_15min:
@@ -232,12 +181,16 @@ def main():
     #
     #
     #return table_day, table_15min
+    #generate paths
+    DATABASE_LOCATION = os.path.join(get_config()['jwql_dir'], 'database')
+    DATABASE_FILE = os.path.join(DATABASE_LOCATION, 'nirspec_database.db')
 
-    #open temporary database and write data!
-    db_file = "/home/daniel/STScI/jwql/jwql/database/miri_database_new.db"
-    conn = sql.create_connection(db_file)
+    #connect to temporary database
+    conn = sql.create_connection(DATABASE_FILE)
 
-    add_day_to_db(conn, table_day)
-    add_15min_to_db(conn, table_15min)
+    process_daysample(conn, table_day)
+    process_15minsample(conn, table_15min)
 
+    #close connection
     sql.close_connection(conn)
+    print("done")
