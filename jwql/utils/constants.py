a39f9dd8f1023a3e5b41e7e374cd68f7d7f72d1f
Matthew Bourque <bourque@stsci.edu>
2019-04-22 21:10:01 -0400

    Added short description for each variable definition

--- jwql/utils/constants.py
+++ jwql/utils/constants.py
@@ -7,7 +7,7 @@ Authors
 
 Use
 ---
-    This variables within this module are inteded to be directly
+    This variables within this module are intended to be directly
     imported, e.g.:
     ::
 
@@ -20,45 +20,58 @@ References
     ``utils.py``
 """
 
+# Possible suffix types for AMI files
 AMI_SUFFIX_TYPES = ['amiavg', 'aminorm', 'ami']
 
+# Defines the x and y coordinates of amplifier boundaries
 AMPLIFIER_BOUNDARIES = {'nircam': {'1': [(0, 0), (512, 2048)], '2': [(512, 0), (1024, 2048)],
                                    '3': [(1024, 0), (1536, 2048)], '4': [(1536, 0), (2048, 2048)]}
                         }
 
+# Possible suffix types for coronograph exposures
 CORONAGRAPHY_SUFFIX_TYPES = ['psfstack', 'psfalign', 'psfsub']
 
+# NIRCam subarrays that use four amps for readout
 FOUR_AMP_SUBARRAYS = ['WFSS128R', 'WFSS64R']
 
+# Possible suffix types for nominal files
 GENERIC_SUFFIX_TYPES = ['uncal', 'cal', 'rateints', 'rate', 'trapsfilled', 'i2d',
                         'x1dints', 'x1d', 's2d', 's3d', 'dark', 'crfints',
                         'crf', 'ramp', 'fitopt', 'bsubints', 'bsub', 'cat']
 
+# Possible suffix types for guider exposures
 GUIDER_SUFFIX_TYPES = ['stream', 'stacked_uncal', 'image_uncal', 'stacked_cal', 'image_cal']
 
+# JWST data products
 JWST_DATAPRODUCTS = ['IMAGE', 'SPECTRUM', 'SED', 'TIMESERIES', 'VISIBILITY',
                      'EVENTLIST', 'CUBE', 'CATALOG', 'ENGINEERING', 'NULL']
 
+# Lowercase JWST instrument names
 JWST_INSTRUMENT_NAMES = sorted(['niriss', 'nircam', 'nirspec', 'miri', 'fgs'])
 
+# JWST instrument names with shorthand notation
 JWST_INSTRUMENT_NAMES_SHORTHAND = {'gui': 'fgs',
                                    'mir': 'miri',
                                    'nis': 'niriss',
                                    'nrc': 'nircam',
                                    'nrs': 'nirspec'}
 
+# Mixed case JWST instrument names
 JWST_INSTRUMENT_NAMES_MIXEDCASE = {'fgs': 'FGS',
                                    'miri': 'MIRI',
                                    'nircam': 'NIRCam',
                                    'niriss': 'NIRISS',
                                    'nirspec': 'NIRSpec'}
 
+# Upper case JWST instrument names
 JWST_INSTRUMENT_NAMES_UPPERCASE = {key: value.upper() for key, value in
                                    JWST_INSTRUMENT_NAMES_MIXEDCASE.items()}
 
+# Astoquery service string for each JWST instrument
 JWST_MAST_SERVICES = ['Mast.Jwst.Filtered.{}'.format(value.title()) for value in
                       JWST_INSTRUMENT_NAMES]
 
+# Available monitor names and their location for each JWST instrument
 MONITORS = {
     'fgs': [('Bad Pixel Monitor', '#')],
     'miri': [('Dark Current Monitor', '#'),
@@ -88,13 +101,17 @@ MONITORS = {
                 ('Instrument Model Updates', '#'),
                 ('Failed-open Shutter Monitor', '#')]}
 
+# NIRCam long wavelength detector names
 NIRCAM_LONGWAVE_DETECTORS = ['NRCA5', 'NRCB5']
 
+# NIRCam short wavelength detector names
 NIRCAM_SHORTWAVE_DETECTORS = ['NRCA1', 'NRCA2', 'NRCA3', 'NRCA4',
                               'NRCB1', 'NRCB2', 'NRCB3', 'NRCB4']
 
+# NIRCam subarrays that use either one or four amps
 SUBARRAYS_ONE_OR_FOUR_AMPS = ['SUBGRISMSTRIPE64', 'SUBGRISMSTRIPE128', 'SUBGRISMSTRIPE256']
 
+# Possible suffix types for time-series exposures
 TIME_SERIES_SUFFIX_TYPES = ['phot', 'whtlt']
 
 # Concatenate all suffix types (ordered to ensure successful matching)
