dd5b61dcea6d5506365f675270768e49daf408ff
Lauren Chambers <lchambers@stsci.edu>
2019-06-04 14:52:04 -0400

    Update changelog and version number for 0.20.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import numpy as np
 from setuptools import setup
 from setuptools import find_packages
 
-VERSION = '0.19.0'
+VERSION = '0.20.0'
 
 AUTHORS = 'Matthew Bourque, Sara Ogaz, Joe Filippazzo, Bryan Hilbert, Misty Cracraft, '
 AUTHORS += 'Graham Kanarek, Johannes Sahlmann, Lauren Chambers, Catherine Martlin'
