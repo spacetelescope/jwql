02e62b5a1523f8078559fb0f7628d58ab624321f
Ben Sunnquist <bsunnquist@stsci.edu>
2019-07-31 17:02:14 -0400

    continued bias monitor work for nircam

--- jwql/instrument_monitors/common_monitors/bias_monitor.py
+++ jwql/instrument_monitors/common_monitors/bias_monitor.py
@@ -15,36 +15,64 @@ Use
         python bias_monitor.py
 """
 
+import logging
+import os
+
 from astropy.io import fits
+from astropy.time import Time
 import numpy as np
 
 from jwql.instrument_monitors.common_monitors.dark_monitor import mast_query_darks   
-from jwql.utils.utils import initialize_instrument_monitor, update_monitor_table
+from jwql.utils.utils import filesystem_path, initialize_instrument_monitor, update_monitor_table
 
 class Bias():
     """Class for executing the bias monitor.
     """
 
     def __init__(self):
-        """Initialize an instance of the ``Bias`` class."""
+        """Initialize an instance of the ``Bias`` class.
+        """
 
     @log_fail
     @log_info
     def run(self):
-        """The main method.  See module docstrings for further
-        details.
+        """The main method.  See module docstrings for further details.
         """
 
         logging.info('Begin logging for bias_monitor')
 
+        # Get the output directory
+        self.output_dir = os.path.join(get_config()['outputs'], 'bias_monitor')
+
+        # query_start is a PLACEHOLDER. Use the current time as the end time for MAST query
+        self.query_start, self.query_end = 57357.0, Time.now().mjd  # a.k.a. Dec 1, 2015 == CV3
+
         # Loop over all instruments
         for instrument in ['nircam']:
             self.instrument = instrument
-            aperture = 'nrca1_full'
-            query_start, query_end = 0, 99999999
 
-            # Query MAST for new data for this instrument
-            new_entries = mast_query_darks(instrument, aperture, query_start, query_end)
+            # PLACEHOLDER Make a list of all possible apertures
+            possible_apertures = ['NRCA1_FULL','NRCA2_FULL','NRCA3_FULL','NRCA4_FULL','NRCA5_FULL',
+                                  'NRCB1_FULL','NRCB2_FULL','NRCB3_FULL','NRCB4_FULL','NRCB5_FULL']
+
+            for aperture in possible_apertures:
+
+                logging.info('')
+                logging.info('Working on aperture {} in {}'.format(aperture, instrument))
+
+                # Query MAST for new data for this instrument/aperture
+                logging.info('\tQuery times: {} {}'.format(self.query_start, self.query_end))
+                new_entries = mast_query_darks(instrument, aperture, self.query_start, self.query_end)
+                logging.info('\tAperture: {}, new entries: {}'.format(self.aperture, len(new_entries)))
+
+                # Get full paths to the files, some dont exist in JWQL filesystem
+                new_filenames = []
+                for file_entry in new_entries:
+                    try:
+                        new_filenames.append(filesystem_path(file_entry['filename']))
+                    except FileNotFoundError:
+                        logging.info('{} does not exist in JWQL filesystem'.format(file_entry['filename']))
+
 
 if __name__ == '__main__':
 
