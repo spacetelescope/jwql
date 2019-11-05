cb7e74c1b6f2a6d7d1d3c4df5ed89c8ee6c1a1d0
Ben Sunnquist <bsunnquist@stsci.edu>
2019-11-05 16:24:34 -0500

    added slider that lets you change the integration that is being displayed

--- jwql/website/apps/jwql/static/js/jwql.js
+++ jwql/website/apps/jwql/static/js/jwql.js
@@ -38,6 +38,11 @@ function change_filetype(type, file_root, num_ints, inst) {
     img.src = jpg_filepath;
     img.alt = jpg_filepath;
 
+    // Reset the slider values
+    document.getElementById("myRange").value = 1
+    document.getElementById("myRange").max = num_ints[type]
+    document.getElementById("slider_val").innerHTML = 1
+
     // Update the number of integrations
     var int_counter = document.getElementById("int_count");
     int_counter.innerHTML = 'Displaying integration 1/' + num_ints[type];
@@ -115,6 +120,62 @@ function change_int(direction, file_root, num_ints) {
     var int_counter = document.getElementById("int_count");
     var int_display = new_integration + 1;
     int_counter.innerHTML = 'Displaying integration ' + int_display + '/' + num_ints;
+
+    // Update the jpg download link
+    document.getElementById("download_jpg").href = jpg_filepath;
+
+    // Update the slider values
+    document.getElementById("myRange").value = new_integration + 1
+    document.getElementById("slider_val").innerHTML = new_integration + 1
+};
+
+ /**
+ * Change the integration number of the displayed image based on the slider value
+ * @param {String} file_root - The rootname of the file
+ * @param {Dict} num_ints - A dictionary whose keys are suffix types and whose
+ *                          values are the number of integrations for that suffix
+ */
+function change_int_slider(file_root, num_ints) {
+
+    // Get the current value of the integration slider
+    var slider = document.getElementById("myRange");
+    new_integration = slider.value - 1;
+
+    // Figure out the current image and integration
+    var suffix = document.getElementById("jpg_filename").innerHTML.split('_');
+    var integration = Number(suffix[suffix.length - 1][5]);
+    var suffix = suffix[suffix.length - 2];
+    var program = file_root.slice(0,7);
+
+    var num_ints = num_ints.replace(/'/g, '"');
+    var num_ints = JSON.parse(num_ints)[suffix];
+
+    // Update the JPG filename based on the slider value
+    var jpg_filename = file_root + '_' + suffix + '_integ' + new_integration + '.jpg'
+    var jpg_filepath = '/static/preview_images/' + program + '/' + jpg_filename
+    document.getElementById("jpg_filename").innerHTML = jpg_filename;
+
+    // Show the appropriate image
+    var img = document.getElementById("image_viewer")
+    img.src = jpg_filepath;
+    img.alt = jpg_filepath;
+
+    // Update the number of integrations
+    var int_counter = document.getElementById("int_count");
+    var int_display = new_integration + 1;
+    int_counter.innerHTML = 'Displaying integration ' + int_display + '/' + num_ints;
+
+    // Update the buttons
+    if (int_display == 1) {
+        document.getElementById("int_after").disabled = false;
+        document.getElementById("int_before").disabled = true;
+    } else if (int_display < num_ints) {
+        document.getElementById("int_after").disabled = false;
+        document.getElementById("int_before").disabled = false;
+    } else if (int_display == num_ints) {
+        document.getElementById("int_after").disabled = true;
+        document.getElementById("int_before").disabled = false;
+    }
 
     // Update the jpg download link
     document.getElementById("download_jpg").href = jpg_filepath;
