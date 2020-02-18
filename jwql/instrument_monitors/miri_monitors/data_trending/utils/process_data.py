abe83cd4dedeb2dd22b8f4a783d442e7a2645bdf
Matthew Bourque <bourque@stsci.edu>
2020-02-18 17:03:40 -0500

    Refactored process_data.py

--- jwql/instrument_monitors/miri_monitors/data_trending/utils/process_data.py
+++ jwql/instrument_monitors/miri_monitors/data_trending/utils/process_data.py
@@ -17,19 +17,11 @@ Use
 
     ::
         from .utils.process_data import whole_day_routine
-        whole_day_routine(raw_daya)
-
-Dependencies
-------------
-
-    - ``sqlite3``
+        whole_day_routine(raw_daya)`
 """
 
 from collections import defaultdict
-import statistics
 import warnings
-
-import sqlite3
 
 from jwql.instrument_monitors.miri_monitors.data_trending.utils import condition
 from jwql.instrument_monitors.miri_monitors.data_trending.utils import mnemonics
@@ -112,19 +104,19 @@ def _extract_filterpos(condition, nominals, ratio_mnemonic, position_mnemonic):
             for ratio in ratio_mnemonic:
                 if (ratio['time'] >= interval[0]) and (ratio['time'] < interval[1]):
                     interval_ratios.append(ratio)
-                elif ratio['time']>= interval[1]:
-                        break
+                elif ratio['time'] >= interval[1]:
+                    break
 
             # Check whether position values are in range of these checkvals
             window = 1
             found_value = False
-            while found_value == False:
+            while found_value is False:
                 for ratio in interval_ratios:
                     if (abs(float(ratio['value']) - nominals.get(position['value'])) < window):
                         found_value = True
-                        position_values[position['value']].append(( ratio['time'], ratio['value']))
+                        position_values[position['value']].append((ratio['time'], ratio['value']))
                         break
-                window +=2
+                window += 2
                 if window > 10:
                     print('ratio error')
                     break
@@ -174,10 +166,10 @@ def _extract_filterpos1(condition, nominals, ratio_mnemonic, position_mnemonic):
                     # Look for ratio values which are in the same time interval
                     # and differ a certain value (here 5mV) from the nominal
                     if (ratio['time'] >= current_position_time) and \
-                        (abs(float(ratio['value']) - nominals.get(position['value'])) < 5):
+                       (abs(float(ratio['value']) - nominals.get(position['value'])) < 5):
 
                         if (ratio['time'] > interval[0]) and (ratio['time'] < interval[1]):
-                            position_values[position['value']].append(( ratio['time'], ratio['value']))
+                            position_values[position['value']].append((ratio['time'], ratio['value']))
 
         else:
             warnings.warn('UNKNOWN Position')
@@ -217,13 +209,13 @@ def once_a_day_routine(mnemonic_data):
     # to dictitonary
     for identifier in mnemonics.mnemonic_cond_1:
         data = _extract_data(condition_1, mnemonic_data.mnemonic(identifier))
-        if data != None:
-            data_dict.update({identifier:data})
+        if data is not None:
+            data_dict.update({identifier: data})
         else:
             print('no data for {}'.format(identifier))
 
     # Under normal use, the following line should be added:
-    #condition.equal(mnemonic_data.mnemonic('IGDP_IT_MIR_SW_STATUS'), 'DETECTOR_READY'),
+    # condition.equal(mnemonic_data.mnemonic('IGDP_IT_MIR_SW_STATUS'), 'DETECTOR_READY'),
     # SW was missing in the trainigs data so I could not use it for a condition.
     condition_set_2 = [
         condition.greater(mnemonic_data.mnemonic('SE_ZIMIRFPEA'), 0.5),
@@ -235,8 +227,8 @@ def once_a_day_routine(mnemonic_data):
     # to dictitonary
     for identifier in mnemonics.mnemonic_cond_2:
         data = _extract_data(condition_2, mnemonic_data.mnemonic(identifier))
-        if data != None:
-            data_dict.update({identifier:data})
+        if data is not None:
+            data_dict.update({identifier: data})
         else:
             print('no data for {}'.format(identifier))
 
@@ -282,108 +274,77 @@ def wheelpos_routine(mnemonic_data):
         mnemonic_data.mnemonic('IMIR_HK_GW14_POS_RATIO'),
         mnemonic_data.mnemonic('IMIR_HK_GW14_CUR_POS'))
 
-    condition_set_GW23 = [                                               \
-    condition.greater(mnemonic_data.mnemonic('IMIR_HK_GW23_POS_VOLT'),250.0)]
-    #setup condition
-    condition_GW23 = condition.condition(condition_set_GW23)
-    gw23 = _extract_filterpos(condition_GW23, mnemonics.gw23_nominals, \
-        mnemonic_data.mnemonic('IMIR_HK_GW23_POS_RATIO'), mnemonic_data.mnemonic('IMIR_HK_GW23_CUR_POS'))
+    condition_GW23 = condition.condition([condition.greater(mnemonic_data.mnemonic('IMIR_HK_GW23_POS_VOLT'), 250.0)])
+    gw23 = _extract_filterpos(
+        condition_GW23,
+        mnemonics.gw23_nominals,
+        mnemonic_data.mnemonic('IMIR_HK_GW23_POS_RATIO'),
+        mnemonic_data.mnemonic('IMIR_HK_GW23_CUR_POS'))
 
-    del condition_GW23
-
-    condition_set_CCC = [                                               \
-    condition.greater(mnemonic_data.mnemonic('IMIR_HK_CCC_POS_VOLT'),250.0)]
-    #setup condition
-    condition_CCC = condition.condition(condition_set_CCC)
-    ccc = _extract_filterpos(condition_CCC, mnemonics.ccc_nominals, \
-        mnemonic_data.mnemonic('IMIR_HK_CCC_POS_RATIO'), mnemonic_data.mnemonic('IMIR_HK_CCC_CUR_POS'))
-
-    del condition_CCC
+    condition_CCC = condition.condition([condition.greater(mnemonic_data.mnemonic('IMIR_HK_CCC_POS_VOLT'), 250.0)])
+    ccc = _extract_filterpos(
+        condition_CCC,
+        mnemonics.ccc_nominals,
+        mnemonic_data.mnemonic('IMIR_HK_CCC_POS_RATIO'),
+        mnemonic_data.mnemonic('IMIR_HK_CCC_CUR_POS'))
 
     return fw, gw14, gw23, ccc
 
 
 def whole_day_routine(mnemonic_data):
-    """Proposed routine for processing data representing a whole day
+    """Routine for processing data representing a whole day
+
     Parameters
     ----------
     mnemonic_data : dict
-        dict holds time and value in a astropy table with correspining identifier as key
-    Return
-    ------
+        dict holds time and value in a astropy table with corresponding
+        identifier as key
+
+    Returns
+    -------
     data_cond_3 : dict
         Holds extracted data with condition 3 applied
     FW_volt : list
-        extracted data for IMIR_HK_FW_POS_VOLT
+        extracted data for ``IMIR_HK_FW_POS_VOLT```
     GW14_volt : list
-        extracted data for IMIR_HK_GW14_POS_VOLT
+        extracted data for ``IMIR_HK_GW14_POS_VOLT``
     GW23_volt : list
-        extracted data for IMIR_HK_GW23_POS_VOLT
+        extracted data for ``IMIR_HK_GW23_POS_VOLT``
     CCC_volt : list
-        extracted data for IMIR_HK_CCC_POS_VOLT
+        extracted data for ``IMIR_HK_CCC_POS_VOLT``
     """
 
-    #abbreviate attribute
-    m = mnemonic_data
     data_dict = dict()
 
-    #########################################################################
-    condition_set_3 = [                                               \
-    condition.greater(mnemonic_data.mnemonic('IMIR_HK_ICE_SEC_VOLT1'), 25.0)]
-    #setup condition
-    condition_3 = condition.condition(condition_set_3)
+    condition_3 = condition.condition([condition.greater(mnemonic_data.mnemonic('IMIR_HK_ICE_SEC_VOLT1'), 25.0)])
 
-    #add filtered engineering values of mnemonics given in list mnemonic_cond_3
-    #to dictitonary
+    # add filtered engineering values of mnemonics given in list mnemonic_cond_3
+    # to dictitonary
     for identifier in mnemonics.mnemonic_cond_3:
         data = _extract_data(condition_3, mnemonic_data.mnemonic(identifier))
-
-        if data != None:
-            data_dict.update({identifier:data})
+        if data is not None:
+            data_dict.update({identifier: data})
         else:
             print('no data for {}'.format(identifier))
 
-    del condition_3
-
-    #########################################################################
-    #extract data for IMIR_HK_FW_POS_VOLT under given condition
-    condition_set_FW = [                                               \
-    condition.greater(mnemonic_data.mnemonic('IMIR_HK_FW_POS_VOLT'),250.0)]
-    #setup condition
-    condition_FW = condition.condition(condition_set_FW)
+    # Extract data for IMIR_HK_FW_POS_VOLT under given condition
+    condition_FW = condition.condition([condition.greater(mnemonic_data.mnemonic('IMIR_HK_FW_POS_VOLT'), 250.0)])
     FW_volt = _extract_data(condition_FW, mnemonic_data.mnemonic('IMIR_HK_FW_POS_VOLT'))
-    data_dict.update({'IMIR_HK_FW_POS_VOLT':FW_volt})
-    del condition_FW
+    data_dict.update({'IMIR_HK_FW_POS_VOLT': FW_volt})
 
-    #extract data for IMIR_HK_GW14_POS_VOLT under given condition
-    condition_set_GW14 = [                                               \
-    condition.greater(mnemonic_data.mnemonic('IMIR_HK_GW14_POS_VOLT'),250.0)]
-    #setup condition
-    condition_GW14 = condition.condition(condition_set_GW14)
+    # Extract data for IMIR_HK_GW14_POS_VOLT under given condition
+    condition_GW14 = condition.condition([condition.greater(mnemonic_data.mnemonic('IMIR_HK_GW14_POS_VOLT'), 250.0)])
     GW14_volt = _extract_data(condition_GW14, mnemonic_data.mnemonic('IMIR_HK_GW14_POS_VOLT'))
-    data_dict.update({'IMIR_HK_GW14_POS_VOLT':GW14_volt})
-    del condition_GW14
+    data_dict.update({'IMIR_HK_GW14_POS_VOLT': GW14_volt})
 
-    #extract data for IMIR_HK_GW23_POS_VOLT under given condition
-    condition_set_GW23 = [                                               \
-    condition.greater(mnemonic_data.mnemonic('IMIR_HK_GW23_POS_VOLT'),250.0)]
-    #setup condition
-    condition_GW23 = condition.condition(condition_set_GW23)
+    # Extract data for IMIR_HK_GW23_POS_VOLT under given condition
+    condition_GW23 = condition.condition([condition.greater(mnemonic_data.mnemonic('IMIR_HK_GW23_POS_VOLT'), 250.0)])
     GW23_volt = _extract_data(condition_GW23, mnemonic_data.mnemonic('IMIR_HK_GW23_POS_VOLT'))
-    data_dict.update({'IMIR_HK_GW23_POS_VOLT':GW23_volt})
-    del condition_GW23
+    data_dict.update({'IMIR_HK_GW23_POS_VOLT': GW23_volt})
 
-    #extract data for IMIR_HK_CCC_POS_VOLT under given condition
-    condition_set_CCC = [                                               \
-    condition.greater(mnemonic_data.mnemonic('IMIR_HK_CCC_POS_VOLT'),250.0)]
-    #setup condition
-    condition_CCC = condition.condition(condition_set_CCC)
+    # Extract data for IMIR_HK_CCC_POS_VOLT under given condition
+    condition_CCC = condition.condition([condition.greater(mnemonic_data.mnemonic('IMIR_HK_CCC_POS_VOLT'), 250.0)])
     CCC_volt = _extract_data(condition_CCC, mnemonic_data.mnemonic('IMIR_HK_CCC_POS_VOLT'))
-    data_dict.update({'IMIR_HK_CCC_POS_VOLT':CCC_volt})
-    del condition_CCC
+    data_dict.update({'IMIR_HK_CCC_POS_VOLT': CCC_volt})
 
     return data_dict
-
-
-if __name__ =='__main__':
-    pass
