3664a220813715bc8ad026dbafc57380941e8117
Matthew Bourque <bourque@stsci.edu>
2020-03-17 16:23:29 -0400

    Removed call to MonitorTA, as it is not implemented yet.

--- jwql/website/apps/jwql/monitor_pages/__init__.py
+++ jwql/website/apps/jwql/monitor_pages/__init__.py
@@ -1,4 +1,3 @@
 from .monitor_dark_bokeh import DarkMonitor
 from .monitor_filesystem_bokeh import MonitorFilesystem
 from .monitor_mast_bokeh import MastMonitor
-from .monitor_ta_bokeh import MonitorTA
