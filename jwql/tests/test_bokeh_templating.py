2929e05968338495fb2637a4a747c1b3a24b93c6
Graham Kanarek <gkanarek@stsci.edu>
2020-09-23 12:35:02 -0400

    implementing bokeh_templating test cases (and a couple updates to the bokeh_templating example)

--- jwql/tests/test_bokeh_templating.py
+++ jwql/tests/test_bokeh_templating.py
@@ -1,34 +1,32 @@
-"""
-This is a minimal example demonstrating how to create a Bokeh app using
-the ``bokeh-templating`` package and the associated YAML template files.
-
-Author
+"""Tests for the ``bokeh_templating`` module.
+Authors
 -------
-
     - Graham Kanarek
-
-Dependencies
-------------
-
-    The user must have PyYAML, Bokeh, and the ``bokeh-templating``
-    packages installed.
+Use
+---
+    These tests can be run via the command line (omit the -s to
+    suppress verbose output to stdout):
+    ::
+        pytest -s test_bokeh_templating.py
 """
 
 import os
 import numpy as np
-
 from jwql.bokeh_templating import BokehTemplate
-
 file_dir = os.path.dirname(os.path.realpath(__file__))
 
 
-class TestBokehApp(BokehTemplate):
-    """This is a minimal ``BokehTemplate`` app."""
+class TestTemplate(BokehTemplate):
+    """
+    A minimal BokehTemplate app for testing purposes. This is adapted from
+    the example included in the ``bokeh_template`` package.
+    """
     
     _embed = True
 
     def pre_init(self):
-        """Before creating the Bokeh interface (by parsing the interface
+        """
+        Before creating the Bokeh interface (by parsing the interface
         file), we must initialize our ``a`` and ``b`` variables, and set
         the path to the interface file.
         """
@@ -36,7 +34,7 @@ class TestBokehApp(BokehTemplate):
         self.a, self.b = 4, 2
 
         self.format_string = None
-        self.interface_file = os.path.join(file_dir, "example_interface.yaml")
+        self.interface_file = os.path.join(file_dir, "test_bokeh_tempating_interface.yaml")
 
     # No post-initialization tasks are required.
     post_init = None
@@ -52,14 +50,24 @@ class TestBokehApp(BokehTemplate):
         return 3. * np.sin(self.b * np.linspace(0, 2 * np.pi, 500))
 
     def controller(self, attr, old, new):
-        """This is the controller function which is used to update the
+        """
+        This is the controller function which is used to update the
         curves when the sliders are adjusted. Note the use of the
         ``self.refs`` dictionary for accessing the Bokeh object
-        attributes."""
+        attributes.
+        """
         self.a = self.refs["a_slider"].value
         self.b = self.refs["b_slider"].value
 
         self.refs["figure_source"].data = {'x': self.x, 'y': self.y}
 
+def test_bokeh_templating():
+    """
+    """
 
-TestBokehApp()
+    test_template = TestTemplate()
+    script, div = test_template.embed('the_figure')
+    
+    assert type(script) == str
+    assert type(div) == str
+    assert "Figure" in script
