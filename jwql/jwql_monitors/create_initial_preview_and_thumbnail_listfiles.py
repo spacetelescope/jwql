4742de4e271f05288794b8cd88867172a56b2b8c
Bryan Hilbert <hilbert@stsci.edu>
2022-03-30 17:33:16 -0400

    PEP8 fixes

--- jwql/jwql_monitors/create_initial_preview_and_thumbnail_listfiles.py
+++ jwql/jwql_monitors/create_initial_preview_and_thumbnail_listfiles.py
@@ -18,14 +18,12 @@ def create_files():
     """Create a new set of listfiles"""
     inst_strings = ['guider', 'nrc', 'miri', 'nis', 'nrs']
 
-
     config = get_config()
     prev_img_dir = config["preview_image_filesystem"]
     thumb_img_dir = config["thumbnail_filesystem"]
 
     # Preview images
-    #/internal/data1/preview_images/jw01014/jw01014001001_02101_00001_guider1_rateints_integ4.jpg
-    for inst_abbrev in ['miri']: #inst_strings:
+    for inst_abbrev in inst_strings:
 
         # Instrument abbreviation to use in output filename
         file_abbrev = inst_abbrev
@@ -41,6 +39,7 @@ def create_files():
         thumb_files = sorted(glob(os.path.join(thumb_img_dir, f'j*/j*{inst_abbrev}*thumb')))
         thumb_listfile = os.path.join(thumb_img_dir, f'thumbnail_inventory_{file_abbrev}.txt')
         write_file(thumb_files, thumb_listfile)
+
 
 def write_file(filelist, output_file):
     """Write a list of filenames to an ascii file
