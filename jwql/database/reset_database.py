7ace74f4980ea1504695d9a1e36a7ce3ab39e656
york@stsci.edu <york@stsci.edu>
2022-09-01 23:05:19 -0400

    typo

--- jwql/database/reset_database.py
+++ jwql/database/reset_database.py
@@ -102,8 +102,9 @@ if __name__ == '__main__':
             else:
                 for monitor in monitor_tables:
                     if monitor != 'anomaly':
-                        base.metadata.drop_all(tables=monitor_tables[monitor])
-                        base.metadata.create_all(tables=monitor_tables[monitor])
+                        tables = [x.__table__ for x in monitor_tables[monitor]]
+                        base.metadata.drop_all(tables=tables)
+                        base.metadata.create_all(tables=tables)
                 print('\nDatabase instance {} has been reset'.format(connection_string))
                 sys.exit(0)
         
@@ -112,7 +113,7 @@ if __name__ == '__main__':
         for table in base_tables:
             if table in check_tables:
                 if (table not in MONITOR_TABLES['anomaly']) or (args.explicit_anomaly):
-                    tables.append(table)
+                    tables.append(table.__table__)
         base.metadata.drop_all(tables=tables)
         base.metadata.create_all(tables=tables)
         print('\nDatabase instance {} has been reset'.format(connection_string))
