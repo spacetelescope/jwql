67ce289719d1a4cfd9c61cb0a22506cceb3d7e9b
Mees Fix <mfix@stsci.edu>
2022-09-07 17:14:43 -0400

    Adding print to debug on server

--- jwql/website/apps/jwql/views.py
+++ jwql/website/apps/jwql/views.py
@@ -991,4 +991,6 @@ def view_image(request, inst, file_root, rewrite=False):
                'total_ints': image_info['total_ints'],
                'form': form}
 
+    print(file_root_list, context['prop_id'], file_root)
+
     return render(request, template, context)
