25258b1c0902f054e33c2b25991b8059f71b8587
york@stsci.edu <york@stsci.edu>
2022-10-03 13:36:42 -0400

    trying to log things

--- jwql/database/reset_database.py
+++ jwql/database/reset_database.py
@@ -113,7 +113,7 @@ if __name__ == '__main__':
                                 print("*****ERROR TYPE*****")
                                 print(type(pe))
                                 print("*****ERROR CODE*****")
-                                print(pe.StatementError)
+                                print(pe.__dict__)
                                 raise pe
                             table.__table__.create()
                 print('\nDatabase instance {} has been reset'.format(connection_string))
@@ -132,7 +132,7 @@ if __name__ == '__main__':
                         print("*****ERROR TYPE*****")
                         print(type(pe))
                         print("*****ERROR CODE*****")
-                        print(pe.StatementError)
+                        print(pe.__dict__)
                         raise pe
                     table.__table__.create()
         print('\nDatabase instance {} has been reset'.format(connection_string))
