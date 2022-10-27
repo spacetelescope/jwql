04dc0bbb34e8879968660cfdee9092d3e185529e
york@stsci.edu <york@stsci.edu>
2022-10-27 11:10:15 -0400

    Added chunked iteration to CR monitor

--- jwql/utils/utils.py
+++ jwql/utils/utils.py
@@ -29,6 +29,7 @@ References
 
 import getpass
 import glob
+import itertools
 import json
 import pyvo as vo
 import os
@@ -625,3 +626,31 @@ def query_unformat(string):
     unsplit_string = string.replace(" ", "_")
 
     return unsplit_string
+
+
+def grouper(iterable, chunksize):
+    """
+    Take a list of items (iterable), and group it into chunks of chunksize, with the
+    last chunk being any remaining items. This allows you to batch-iterate through a 
+    potentially very long list without missing any items, and where each individual
+    iteration can involve a much smaller number of files. Particularly useful for 
+    operations that you want to execute in batches, but don't want the batches to be too
+    long.
+    
+    Examples
+    --------
+    
+    grouper([1, 2, 3, 4, 5], 2)
+    produces
+    (1, 2), (3, 4), (5, )
+    
+    grouper([1, 2, 3, 4, 5], 6)
+    produces
+    (1, 2, 3, 4, 5)
+    """
+    it = iter(iterable)
+    while True:
+        chunk = tuple(itertools.islice(it, chunksize))
+        if not chunk:
+            return
+        yield chunk
