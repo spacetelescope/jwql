abd5eb9ba649d877cb1cc20f80f0d0ec994645f4
Bryan Hilbert <hilbert@stsci.edu>
2023-01-26 16:59:36 -0500

    Remove bokeh templating files

--- jwql/website/apps/jwql/monitor_pages/__init__.py
+++ jwql/website/apps/jwql/monitor_pages/__init__.py
@@ -1,7 +1,6 @@
 from .monitor_bad_pixel_bokeh import BadPixelMonitor
 from .monitor_bias_bokeh import BiasMonitor
 from .monitor_cosmic_rays_bokeh import CosmicRayMonitor
-#from .monitor_dark_bokeh import DarkMonitorPlots
 from .monitor_filesystem_bokeh import MonitorFilesystem
 from .monitor_mast_bokeh import MastMonitor
 from .monitor_readnoise_bokeh import ReadnoiseMonitor
