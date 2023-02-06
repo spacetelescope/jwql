90d9fffb37f619c0c99d6c74083aa4346c284224
Melanie Clarke <mclarke@stsci.edu>
2023-02-06 16:05:49 -0500

    Get context processor test running

--- jwql/website/apps/jwql/tests/test_context_processors.py
+++ jwql/website/apps/jwql/tests/test_context_processors.py
@@ -31,11 +31,18 @@ if not ON_GITHUB_ACTIONS:
     from jwql.utils.utils import get_base_url
 
 
-@skipIf(True, "Needs updating: throws errors for missing settings and apps")
 @skipIf(ON_GITHUB_ACTIONS, "Can't access webpage without VPN access")
 class TestBaseContext(TestCase):
     def test_base_context(self):
         """Tests the ``base_context`` function."""
+
+        # These lines are needed in order to use the Django models in a standalone
+        # script (as opposed to code run as a result of a webpage request). If these
+        # lines are not run, the script will crash when attempting to import the
+        # Django models in the line below.
+        from django import setup
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jwql.website.jwql_proj.settings")
+        setup()
 
         client = Client()
         request = client.get('{}/about/'.format(get_base_url()))
@@ -44,6 +51,6 @@ class TestBaseContext(TestCase):
 
         assert isinstance(context, dict)
 
-        keys = ['inst_list', 'tools', 'user', 'version']
+        keys = ['inst_list', 'tools', 'version']
         for key in keys:
             assert key in context
