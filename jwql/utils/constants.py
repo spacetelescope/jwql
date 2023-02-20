fbd34488db0f549831bae067f2ebec4549bed317
Melanie Clarke <mclarke@stsci.edu>
2023-02-20 12:14:33 -0500

    Add viewed to report for all instruments

--- jwql/utils/constants.py
+++ jwql/utils/constants.py
@@ -470,10 +470,14 @@ READPATT_PER_INSTRUMENT = {'fgs': ['FGS', 'FGSRAPID', 'FGS60', 'FGS840', 'FGS837
                                        'NRSRAPIDD2', 'NRSRAPIDD6']}
 
 
-REPORT_KEYS_PER_INSTRUMENT = {'fgs': ['proposal', 'obsnum', 'exptypes', 'obsstart', 'obsend'],
-                              'miri': ['proposal', 'obsnum', 'exptypes', 'obsstart', 'obsend'],
-                              'nircam': ['proposal', 'obsnum', 'exptypes', 'obsstart', 'obsend'],
-                              'niriss': ['proposal', 'obsnum', 'exptypes', 'obsstart', 'obsend'],
+REPORT_KEYS_PER_INSTRUMENT = {'fgs': ['proposal', 'obsnum', 'exptypes',
+                                      'obsstart', 'obsend', 'viewed'],
+                              'miri': ['proposal', 'obsnum', 'exptypes',
+                                       'obsstart', 'obsend', 'viewed'],
+                              'nircam': ['proposal', 'obsnum', 'exptypes',
+                                         'obsstart', 'obsend', 'viewed'],
+                              'niriss': ['proposal', 'obsnum', 'exptypes',
+                                         'obsstart', 'obsend', 'viewed'],
                               'nirspec': ['exptypes', 'viewed']}
 
 SUBARRAYS_ONE_OR_FOUR_AMPS = ['SUBGRISMSTRIPE64', 'SUBGRISMSTRIPE128', 'SUBGRISMSTRIPE256']
