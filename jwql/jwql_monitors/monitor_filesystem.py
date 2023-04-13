7f92cc8ec920699c2416a3e0384d6e512162f2ea
Bryan Hilbert <hilbert@stsci.edu>
2023-04-13 14:41:24 -0400

    Pass filter img counts as list

--- jwql/jwql_monitors/monitor_filesystem.py
+++ jwql/jwql_monitors/monitor_filesystem.py
@@ -674,11 +674,13 @@ def update_characteristics_database(char_info):
 
     # Add data to filesystem_instrument table
     for instrument in ['nircam', 'niriss', 'nirspec', 'miri']:
+        optics = [e[0] for e in char_info[instrument]]
+        values = [e[1] for e in char_info[instrument]]
         new_record = {}
         new_record['date'] = now
         new_record['instrument'] = instrument
-        new_record['filter_pupil'] = list(char_info[instrument].keys())
-        new_record['obs_per_filter_pupil'] = list(char_info[instrument].values())
+        new_record['filter_pupil'] = optics
+        new_record['obs_per_filter_pupil'] = values
         with engine.begin() as connection:
             connection.execute(
                 FilesystemCharacteristics.__table__.insert(), new_record)
