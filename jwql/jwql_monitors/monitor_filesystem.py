bba5f08bcc8e1055dbcf5c0c19d640f2bebba095
Bryan Hilbert <hilbert@stsci.edu>
2023-04-14 12:47:31 -0400

    Remove unused bokeh templating file

--- jwql/jwql_monitors/monitor_filesystem.py
+++ jwql/jwql_monitors/monitor_filesystem.py
@@ -11,6 +11,7 @@ Authors
     - Misty Cracraft
     - Sara Ogaz
     - Matthew Bourque
+    - Bryan Hilbert
 
 Use
 ---
--- jwql/website/apps/jwql/bokeh_dashboard.py
+++ jwql/website/apps/jwql/bokeh_dashboard.py
@@ -11,6 +11,7 @@ Authors
 -------
 
     - Mees B. Fix
+    - Bryan Hilbert
 
 Use
 ---
