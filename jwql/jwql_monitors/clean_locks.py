ad2bc703832a225792cbf31a883c46fe499e4066
Bradley Sappington <bsappington@stsci.edu>
2023-05-31 14:36:59 -0400

    add (p)ath argument

--- jwql/jwql_monitors/clean_locks.py
+++ jwql/jwql_monitors/clean_locks.py
@@ -20,18 +20,20 @@ Authors
 Use
 ---
 
-    This script is intended to be run as an hourly cronjob as protection for generate_preview_images.py
+    This script is intended to be run as a frequent cronjob as protection for generate_preview_images.py
 
     ::
 
         python clean_locks.py
+        or
+        python clean_locks.py -p "/directory/to/check"
 """
+import argparse
 import glob
 import os
 
 from psutil import pid_exists
 
-# SAPP TODO - MAKE NOTE TO add psutil to env in dev/test/prod
 # SAPP TODO - Consider making LOCK_FILE_PID_KEY in protect_module.py to keep this method safe if there is ever a change to "Process Id = ".
 
 
@@ -44,10 +46,9 @@ def clean_lock_file(filename):
                 os.remove(filename)
                 print(f"File '{filename}' deleted successfully.")  # SAPP TODO -DEL THIS
                 return f"File '{filename}' was never deleted and associated PID no longer running, deleted successfully."
-            else:
-                print(f"File '{filename}' does not exist.")  # SAPP TODO -DEL THIS
+        return ""
     except SyntaxError as e:
-        return e
+        return str(e)
 
 
 def get_all_lock_files(search_path="."):
@@ -55,25 +56,50 @@ def get_all_lock_files(search_path="."):
     return lock_files
 
 
+def parse_args():
+    """ Parse command line arguments.
+
+    Parameters:
+
+    Returns:
+        arguments: argparse.Namespace object
+            An object containing all of the added arguments.
+
+    Outputs:
+        Nothing
+    """
+    path_help = ('Directory where you would like to search for *.lock files.'
+                 ' Default is current directory')
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p',
+                        type=str,
+                        help=path_help,
+                        action='store',
+                        required=False,
+                        default='.')
+
+    arguments = parser.parse_args()
+
+    return arguments
+
+
 def retreive_pid_from_lock_file(filename):
     # Lock file format is established in `jwql/utils/protect_module.py::lock_module`
-
     with open(filename, 'r') as file:
         for line in file:
             if "Process Id = " in line:
                 number_index = line.index("Process Id = ") + len("Process Id = ")
                 number = line[number_index:].strip()
-                print(f"YES PID FOUND IN {filename}!!!!")  # SAPP TODO -DEL THIS
                 return int(number)
 
-    print(f"NO PID FOUND IN {filename}")  # SAPP TODO -DEL THIS
-    raise SyntaxError(f"No PID found in {filename}")
+    raise SyntaxError(f"No PID found in {filename} - Please manually investigate and delete if appropriate")
 
 
 if __name__ == '__main__':
     email_data = []
-
-    lock_files = get_all_lock_files()
+    args = parse_args()
+    lock_files = get_all_lock_files(args.p)
 
     for file in lock_files:
         notify_str = clean_lock_file(file)
@@ -85,3 +111,18 @@ if __name__ == '__main__':
     print("EMAIL THIS: ")
     for line in email_data:
         print(line)
+
+        # SAPP TODO - implement this
+"""
+        deliverer = '{}@stsci.edu'.format(user)
+
+        message = MIMEText(success_message)
+        message['Subject'] = 'JWQL ACTION FROM `CLEAN_LOCKS` SCRIPT'
+        message['From'] = deliverer
+        message['To'] = 'redcat@stsci.edu'
+        message['CC'] = deliverer
+
+        s = smtplib.SMTP('smtp.stsci.edu')
+        s.send_message(message)
+        s.quit()
+    """
