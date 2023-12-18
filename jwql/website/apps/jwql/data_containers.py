8087142e4cf4e2591ff04a6187d0834145492528
Bradley Sappington <bsappington@stsci.edu>
2023-12-18 10:56:23 -0500

    pep8 fixes

--- jwql/website/apps/jwql/data_containers.py
+++ jwql/website/apps/jwql/data_containers.py
@@ -407,9 +407,9 @@ def get_available_suffixes(all_suffixes, return_untracked=True):
     for poss_suffix in EXPOSURE_PAGE_SUFFIX_ORDER:
         if 'crf' not in poss_suffix:
             if (poss_suffix in all_suffixes
-                and poss_suffix not in suffixes):
-                    suffixes.append(poss_suffix)
-                    untracked_suffixes.remove(poss_suffix)
+            and poss_suffix not in suffixes):
+                suffixes.append(poss_suffix)
+                untracked_suffixes.remove(poss_suffix)
         else:
             # EXPOSURE_PAGE_SUFFIX_ORDER contains crf and crfints,
             # but the actual suffixes in the data will be e.g. o001_crf,
@@ -417,8 +417,8 @@ def get_available_suffixes(all_suffixes, return_untracked=True):
             # So in this case, we strip the e.g. o001 from the
             # suffixes and check which list elements match.
             for image_suffix in all_suffixes:
-                if (image_suffix.endswith(poss_suffix) and
-                    image_suffix not in suffixes):
+                if (image_suffix.endswith(poss_suffix)
+                and image_suffix not in suffixes):
                     suffixes.append(image_suffix)
                     untracked_suffixes.remove(image_suffix)
 
@@ -1399,7 +1399,7 @@ def get_proposals_by_category(instrument):
 
     service = "Mast.Jwst.Filtered.{}".format(instrument)
     params = {"columns": "program, category",
-              "filters": [{'paramName':'instrume', 'values':[instrument]}]}
+              "filters": [{'paramName': 'instrume', 'values': [instrument]}]}
     response = Mast.service_request_async(service, params)
     results = response[0].json()['data']
 
@@ -2086,7 +2086,7 @@ def thumbnails_query_ajax(rootnames):
     proposals = [data_dict['file_data'][rootname]['filename_dict']['program_id'] for
                  rootname in list(data_dict['file_data'].keys())]
     visits = [data_dict['file_data'][rootname]['filename_dict']['visit'] for
-                 rootname in list(data_dict['file_data'].keys())]
+              rootname in list(data_dict['file_data'].keys())]
 
     dropdown_menus = {'instrument': instruments,
                       'detector': sorted(detectors),
