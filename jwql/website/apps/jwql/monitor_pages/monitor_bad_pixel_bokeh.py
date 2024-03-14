c7ed1c6dcbaa64e08007c644886bb64a0d1f6b01
Mees Fix <mfix@stsci.edu>
2024-03-14 09:48:48 -0400

    pep8

--- jwql/website/apps/jwql/monitor_pages/monitor_bad_pixel_bokeh.py
+++ jwql/website/apps/jwql/monitor_pages/monitor_bad_pixel_bokeh.py
@@ -162,7 +162,7 @@ class BadPixelPlots():
         template_dir = os.path.join(os.path.dirname(__file__), '../templates')
         template_file = os.path.join(template_dir, 'bad_pixel_monitor_savefile_basic.html')
         temp_vars = {'inst': self.instrument, 'plot_script': script, 'plot_div': div}
-        self._html = file_html(tabs, CDN, title=f'{self.instrument} bad pix monitor', 
+        self._html = file_html(tabs, CDN, title=f'{self.instrument} bad pix monitor',
                                template=template_file, template_variables=temp_vars)
 
         # Modify the html such that our Django-related lines are kept in place,
